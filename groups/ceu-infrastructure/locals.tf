8f2ab0a821548944b3566cc40174ce6a51f4168d
Marty Fox <martin.fox@kainos.com>
2020-12-10 13:53:28 +0000

    missed ewf reference, updating to ceu

--- groups/ceu-infrastructure/locals.tf
+++ groups/ceu-infrastructure/locals.tf
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------
 locals {
   admin_cidrs  = values(data.vault_generic_secret.internal_cidrs.data)
-  ewf_rds_data = data.vault_generic_secret.ewf_rds.data
+  ceu_rds_data = data.vault_generic_secret.ceu_rds.data
 
   internal_fqdn = format("%s.%s.aws.internal", split("-", var.aws_account)[1], split("-", var.aws_account)[0])
 
