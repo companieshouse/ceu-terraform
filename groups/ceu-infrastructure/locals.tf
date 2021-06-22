f87d716cc9a8685f977530d1570f57e27fad711f
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2021-06-22 12:02:39 +0100

    Added initial locals to support CEU BEP

--- groups/ceu-infrastructure/locals.tf
+++ groups/ceu-infrastructure/locals.tf
@@ -4,10 +4,36 @@
 locals {
   admin_cidrs  = values(data.vault_generic_secret.internal_cidrs.data)
   ceu_rds_data = data.vault_generic_secret.ceu_rds.data
+  ceu_bep_data = data.vault_generic_secret.ceu_bep_data.data_json
+  ceu_ec2_data = data.vault_generic_secret.ceu_ec2_data.data
+
+  kms_keys_data          = data.vault_generic_secret.kms_keys.data
+  security_kms_keys_data = data.vault_generic_secret.security_kms_keys.data
+  logs_kms_key_id        = local.kms_keys_data["logs"]
+  ssm_kms_key_id         = local.security_kms_keys_data["session-manager-kms-key-arn"]
+
+  security_s3_data            = data.vault_generic_secret.security_s3_buckets.data
+  session_manager_bucket_name = local.security_s3_data["session-manager-bucket-name"]
 
   internal_fqdn = format("%s.%s.aws.internal", split("-", var.aws_account)[1], split("-", var.aws_account)[0])
 
   rds_ingress_cidrs = concat(local.admin_cidrs, var.rds_onpremise_access)
+
+  bep_cw_logs = { for log, map in var.bep_cw_logs : log => merge(map, { "log_group_name" = "${var.application}-bep-${log}" }) }
+  bep_log_groups = compact([for log, map in local.bep_cw_logs : lookup(map, "log_group_name", "")])
+
+  ceu_bep_ansible_inputs = {
+    s3_bucket_releases         = local.s3_releases["release_bucket_name"]
+    s3_bucket_configs          = local.s3_releases["config_bucket_name"]
+    heritage_environment       = var.environment
+    version                    = var.bep_app_release_version
+    default_nfs_server_address = var.nfs_server
+    mounts_parent_dir          = var.nfs_mount_destination_parent_dir
+    mounts                     = var.nfs_mounts
+    region                     = var.aws_region
+    cw_log_files               = local.bep_cw_logs
+    cw_agent_user              = "root"
+  }
 
   default_tags = {
     Terraform   = "true"
