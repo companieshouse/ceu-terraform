f3346928087ea4081d57e8a12dab80e0f34f0017
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2021-06-22 11:57:02 +0100

    Added initial data to support CEU BEP

--- groups/ceu-infrastructure/data.tf
+++ groups/ceu-infrastructure/data.tf
@@ -39,3 +39,46 @@ data "vault_generic_secret" "ceu_rds" {
 data "vault_generic_secret" "internal_cidrs" {
   path = "aws-accounts/network/internal_cidr_ranges"
 }
+
+#-----------------
+# CEU Backend Data
+#-----------------
+data "aws_ami" "ceu_bep" {
+  owners      = [data.vault_generic_secret.account_ids.data["heritage-development"]]
+  most_recent = var.bep_ami_name == "ceu-backend-*" ? true : false
+
+  filter {
+    name = "name"
+    values = [
+      var.bep_ami_name,
+    ]
+  }
+
+  filter {
+    name = "state"
+    values = [
+      "available",
+    ]
+  }
+}
+
+data "template_file" "bep_userdata" {
+  template = file("${path.module}/templates/bep_user_data.tpl")
+
+  vars = {
+    REGION             = var.aws_region
+    CEU_BACKEND_INPUTS = local.ceu_bep_data
+    ANSIBLE_INPUTS     = jsonencode(local.ceu_bep_ansible_inputs)
+    CEU_CRON_ENTRIES   = var.account == "hlive" ? "#No Entries" : templatefile("${path.module}/templates/bep_cron.tpl", { "USER" = "", "PASSWORD" = "" })
+  }
+}
+
+data "template_cloudinit_config" "bep_userdata_config" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = data.template_file.bep_userdata.rendered
+  }
+}
