958ea57676bd925a5375e71b8019f206856d8966
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2022-05-11 15:16:04 +0100

    Add RDS start/stop schedules for Dev and Staging

--- groups/ceu-infrastructure/variables.tf
+++ groups/ceu-infrastructure/variables.tf
@@ -101,6 +101,24 @@ variable "rds_backup_window" {
   description = "A backup window that allows AWS to backup your RDS instance e.g. `03:00-06:00`"
 }
 
+variable "rds_schedule_enable" {
+  type        = bool
+  default     = false
+  description = "Controls whether an RDS start/stop schedule is created (true) or not (false)"
+}
+
+variable "rds_start_schedule" {
+  type        = string
+  default     = ""
+  description = "The SSM cron expression to define when the RDS instance should be started"
+}
+
+variable "rds_stop_schedule" {
+  type        = string
+  default     = ""
+  description = "The SSM cron expression to define when the RDS instance should be stopped"
+}
+
 # ------------------------------------------------------------------------------
 # RDS Engine Type Variables
 # ------------------------------------------------------------------------------
